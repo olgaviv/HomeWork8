33d1230c63f008ca763be95e19530668fc345988
olga <olgaviv@mail.ru>
2022-11-23 10:23:39 +0700

    third work

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -117,3 +117,67 @@ Console.WriteLine();
 Console.WriteLine($"\n{minSumLine} - строкa с наименьшей суммой ({sumLine}) элементов ");
 
 */
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+ int[,]CreateRandom2Array(int rows, int columns, int minValue, int maxValue )
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+         for(int j = 0; j < columns; j++)
+        
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+             
+        return array;
+}
+void Show2Array(int [,] array)
+{
+    for(int i = 0; i< array.GetLength(0); i++)
+    {
+        for(int j=0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }
+}
+void MultiplyMatrix(int[,] array1, int[,] array2, int[,] resultArray)
+{
+  for (int i = 0; i < resultArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultArray.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < array1.GetLength(1); k++)
+      {
+        sum += array1[i,k] * array2[k,j];
+      }
+      resultArray[i,j] = sum;
+    }
+  }
+}
+
+
+Console.Write("Input a number of rows:");
+int m1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value:");
+int min1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value:");
+int max1 = Convert.ToInt32(Console.ReadLine());
+int[,] myArray1 = CreateRandom2Array(m1, n1, min1, max1);
+Show2Array (myArray1);
+Console.Write("Input a number of rows:");
+int m2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value:");
+int min2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value:");
+int max2 = Convert.ToInt32(Console.ReadLine());
+int[,] myArray2 = CreateRandom2Array(m2, n2, min2, max2);
+int[,] resultArray = new int[m1,n2];
+
+
+Show2Array (myArray2);
+MultiplyMatrix(myArray1, myArray2, resultArray);
+Console.WriteLine($"Произведение первой и второй матриц:");
+Show2Array (resultArray);
